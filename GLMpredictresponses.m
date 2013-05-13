e756d72539e72455565653959db66b10b05e39fa
Kendrick Kay <kendrick@post.harvard.edu>
2013-05-12 20:51:36 -0700

    update to indicate fractional values in design matrix are allowed; allow <design> to specify onset times; add <tr>,<numtimepoints> as inputs

--- GLMpredictresponses.m
+++ GLMpredictresponses.m
@@ -1,6 +1,6 @@
-function responses = GLMpredictresponses(model,design,dimdata)
+function responses = GLMpredictresponses(model,design,tr,numtimepoints,dimdata)
 
-% function responses = GLMpredictresponses(model,design,dimdata)
+% function responses = GLMpredictresponses(model,design,tr,numtimepoints,dimdata)
 %
 % <model> is one of the following:
 %   A where A is X x Y x Z x conditions x time with the timecourse of the 
@@ -10,28 +10,38 @@ function responses = GLMpredictresponses(model,design,dimdata)
 %     response of each voxel to each condition
 %   Note that in both of these cases, the first time point is assumed to be 
 %   coincident with condition onset.
-% <design> is the experimental design with dimensions time x conditions.
-%   Each column should be zeros except for ones indicating condition onsets.
-%   Can be a cell vector whose elements correspond to different runs.
-%   Different runs can have different numbers of time points.
+% <design> is the experimental design.  There are three possible cases:
+%   1. A where A is a matrix with dimensions time x conditions.
+%      Each column should be zeros except for ones indicating condition onsets.
+%      (Fractional values in the design matrix are also allowed.)
+%   2. {A1 A2 A3 ...} where each of the A's are like the previous case.
+%      The different A's correspond to different runs, and different runs
+%      can have different numbers of time points.
+%   3. {{C1_1 C2_1 C3_1 ...} {C1_2 C2_2 C3_2 ...} ...} where Ca_b
+%      is a vector of onset times for condition a in run b.  Time starts at 0 
+%      and is coincident with the acquisition of the first volume.  This case 
+%      is compatible only with the common-HRF <model>.
+% <tr> is the sampling rate in seconds
+% <numtimepoints> is a vector with the number of time points in each run
 % <dimdata> indicates the dimensionality of the voxels.
 %   A value of 3 indicates X x Y x Z, and a value of 1 indicates XYZ.
 %
-% Given the model (<model>) and the experimental design (<design>),
-% compute the predicted time-series response.
+% Given various inputs, compute the predicted time-series response.
 %
 % Return:
 % <responses> as X x Y x Z x time or a cell vector of elements that are 
-%   each X x Y x Z x time.  The format of <responses> will mirror the format
-%   of <design> (for example, if <design> is a cell vector, <responses> will 
-%   also be a cell vector).
+%   each X x Y x Z x time.  The format of <responses> will be a matrix in the
+%   case that <design> is a matrix (case 1) and will be a cell vector in
+%   the other cases (cases 2 and 3).
 %
 % History:
+% - 2013/05/12: allow <design> to specify onset times; add <tr>,<numtimepoints> as inputs
+% - 2013/05/12: update to indicate fractional values in design matrix are allowed.
 % - 2012/12/03: *** Tag: Version 1.02 ***
 % - 2012/11/2 - Initial version.
 
 % calc
-iscellcase = iscell(design);
+ismatrixcase = ~iscell(design);
 dimtime = dimdata + 2;
 if iscell(model)
   xyzsize = sizefull(model{2},dimdata);
@@ -48,38 +58,76 @@ end
 responses = {};
 for p=1:length(design)
 
-  % number of time points in the run
-  ntime = size(design{p},1);
+  % if onset-time case
+  if iscell(design{p})
 
-  % case of shared HRF
-  if iscell(model)
-    
-    % convolve with HRF
-    temp = conv2(full(design{p}),model{1});  % make full just in case design is sparse
+    % check that we have the case of common-HRF model
+    assert(iscell(model));
 
-    % extract desired subset of time-series
-    temp = temp(1:ntime,:);  % time x conditions
-
-    % weight by the amplitudes
-    responses{p} = reshape((temp * squish(model{2},dimdata)')',[xyzsize ntime]);  % X x Y x Z x time
+    % calc
+    alltimes = linspacefixeddiff(0,tr,numtimepoints(p));
+    hrftimes = linspacefixeddiff(0,tr,length(model{1}));
   
-  % case of individual timecourses
+    % loop over conditions
+    temp = zeros(numtimepoints(p),length(design{p}));  % this will be time x conditions
+    for q=1:length(design{p})
+
+      % onset times for qth condition in run p
+      otimes = design{p}{q};
+    
+      % intialize
+      yvals = 0;
+    
+      % loop over onset times
+      for r=1:length(otimes)
+        
+        % interpolate to find values at the data sampling time points
+        yvals = yvals + interp1(otimes(r) + hrftimes,model{1}',alltimes,'cubic',0);
+
+      end
+
+      % record
+      temp(:,q) = yvals;
+
+    end
+    
+    % weight by the amplitudes
+    responses{p} = reshape((temp * squish(model{2},dimdata)')',[xyzsize numtimepoints(p)]);  % X x Y x Z x time
+  
+  % if regular matrix case
   else
 
-    % length of each timecourse (L)
-    len = size(model,dimtime);
+    % case of shared HRF
+    if iscell(model)
     
-    % expand design matrix using delta functions
-    temp = constructstimulusmatrices(design{p}',0,len-1,0);  % time x L*conditions
+      % convolve with HRF
+      temp = conv2(full(design{p}),model{1});  % make full just in case design is sparse
 
-    % weight design matrix by the timecourses
-    responses{p} = reshape((temp * squish(permute(squish(model,dimdata),[3 2 1]),2))',[xyzsize ntime]);  % X x Y x Z x time
+      % extract desired subset of time-series
+      temp = temp(1:numtimepoints(p),:);  % time x conditions
+
+      % weight by the amplitudes
+      responses{p} = reshape((temp * squish(model{2},dimdata)')',[xyzsize numtimepoints(p)]);  % X x Y x Z x time
+  
+    % case of individual timecourses
+    else
+
+      % length of each timecourse (L)
+      len = size(model,dimtime);
+    
+      % expand design matrix using delta functions
+      temp = constructstimulusmatrices(design{p}',0,len-1,0);  % time x L*conditions
+
+      % weight design matrix by the timecourses
+      responses{p} = reshape((temp * squish(permute(squish(model,dimdata),[3 2 1]),2))',[xyzsize numtimepoints(p)]);  % X x Y x Z x time
+
+    end
 
   end
 
 end
 
 % undo cell if necessary
-if ~iscellcase
+if ismatrixcase
   responses = responses{1};
 end
